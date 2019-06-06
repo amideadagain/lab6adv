ae2195f3621df70c55325532924209fb77bd7f5a
kote6725@gmail.com <kote6725@gmail.com>
2019-06-07 00:47:47 +0300

    Добавьте файлы проекта.

---
+++ lab6adv/lab6adv.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+
+	struct Client
+	{
+		std::string name;
+		int price;
+		int month;
+		int futmonth;
+		void save()
+		{
+			std::ofstream file("D:\ssss.txt");
+			file << "Name :" << name << std::endl;
+			file << "Prise :" << price << std::endl;
+			file << "Month :" << month << std::endl;
+			file << "Future Month :" << futmonth << std::endl;
+		}
+	};
+
+	Client client
+	{
+		"sdgdgsdg",
+		4,
+		4,
+		4
+	};
+	client.save();
+	
+}
